edd7044140c45684acd4638ae9e286eb745ce12c
luiz <luizmarcelolm@hotmail.com>
2024-03-30 14:32:57 -0300

    alt

--- port_react_hooks/src/components/section/Presentation.jsx
+++ port_react_hooks/src/components/section/Presentation.jsx
@@ -5,10 +5,10 @@ import { useEffect, useState } from 'react'
 function Presentation(){
 
     const [text, setText] = useState('');
-    const toRotate = ['Em 2013 me formei em Engenharia da Computação e iniciei minha carreira em T.I', 'Em 2014 mudei de cidade e acabei info para o ramo de manutenção em elevadores.', 'Em 2020 iniciei minha jornada em busca de retornar a área de programação, iniciei meus estudos novamente construindo projetos com diversas tecnologias.', 'Em 2024 iniciei curso de Desenvolvedor FullStack na Escola DNC onde estou aprendendo muito e me sinto preparado para o mercado de trabalho. '];
+    const toRotate = ['Em 2013 me formei em Engenharia da Computação e iniciei minha carreira em T.I', 'Em 2014 mudei de cidade e acabei indo para o ramo de manutenção em elevadores.', 'Em 2020 iniciei minha jornada em busca de retornar a área de programação, iniciei meus estudos novamente construindo projetos com diversas tecnologias.', 'Em 2024 iniciei curso de Desenvolvedor FullStack na Escola DNC onde estou aprendendo muito e me sinto preparado para o mercado de trabalho. '];
     const [loop, setLoop] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
-    const period = 80;
+    const period = 90;
     const [delta, setDelta] = useState(100)  
 
     useEffect(()=>{
@@ -18,21 +18,21 @@ function Presentation(){
         return()=>{clearInterval(ticher)}
     }, [text])
 
-    const toType = () =>{
+    const toType = () =>{          
        let i = loop % toRotate.length;
        let fullText = toRotate[i];
        let updatedText = isDeleting ? fullText.substring(0, text.length-1) : fullText.substring(0, text.length+1)  
-
        setText(updatedText);
 
        if(!isDeleting && updatedText === fullText){
            setIsDeleting(true);
            setDelta(period);
-       }else if(isDeleting && updatedText === ''){
+       }else if(isDeleting && updatedText === '') {
              setIsDeleting(false);
              setDelta(period);
              setLoop(loop+1);
        }
+
     }
 
 
