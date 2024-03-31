1e28fb93335da529da70a7228bcbf4877d611a77
luiz <luizmarcelolm@hotmail.com>
2024-03-31 17:27:01 -0300

    alt

--- port_react_hooks/src/components/section/Presentation.jsx
+++ port_react_hooks/src/components/section/Presentation.jsx
@@ -5,10 +5,14 @@ import { useEffect, useState } from 'react'
 function Presentation(){
 
     const [text, setText] = useState('');
-    const toRotate = ['2013', '2014'];
+    const toRotate = ['EM (2008) INICIEI O CURSO DE ENGENHARIA DA COMPUTAÇÃO, ONDE INICIEI MINHA PAIXÃO POR TECNOLOGIA, DURANTE O CURSO TRABALHEI EM DUAS EMPRESAS COMO ESTAGIÁRIO EM T.I ATUANDO NO SUPORTE TÉCNICO.', 
+                      'EM (2013) ME FORMEI EM ENGENHARIA DA COMPUTAÇÃO E COMECEI A TRABALHAR COM MANUTENÇÃO E SUPORTE TÉCNICO EM CAMPO. ', 
+                      'EM (2014) ACABEI MUDANDO DE CIDADE E COMECEI A TRABALHAR COM MANUTENÇÃO EM ELEVADORES.', 
+                      'EM (2020) RETOMEI MEUS ESTUDOS E COMECEI A ME DEDICAR A PROGRAMAÇÃO.'];
     const [loop, setLoop] = useState(0);
-   // const [isDeleting, setIsDeleting] = useState(false);
-    const period = 90;
+    const [isDeleting, setIsDeleting] = useState(false);
+    const period = 500;
+    const period2 = 10000;
     const [delta, setDelta] = useState(100)  
 
     useEffect(()=>{
@@ -21,15 +25,16 @@ function Presentation(){
     const toType = () =>{          
        let i = loop % toRotate.length;
        let fullText = toRotate[i];
-       let updatedText = fullText.substring(0, text.length+1)  
+      let updatedText = isDeleting ? fullText.substring(text.length) : fullText.substring(text.length)  
        setText(updatedText);
 
        if(!isDeleting && updatedText === fullText){
            setIsDeleting(true);
-           setDelta(period);
+           setDelta(period2);
        }else if(isDeleting && updatedText === '') {
              setIsDeleting(false);
              setDelta(period);
+             
              setLoop(loop+1);
        }
     }
@@ -40,15 +45,15 @@ function Presentation(){
             <h1>Sobre mim</h1>
             <div className={styles.div}>
                 <ul>
-                    <li>{text}</li>
+                    <li>2008</li>
+                    <li>2013</li>
                     <li>2014</li>
                     <li>2020</li>
-                    <li>2024</li>
                 </ul>
             </div>
             <img src={imagem}/>
             <div className={styles.divText}>
-                  <p>" teste "</p> 
+                  <p>"  {text}  "</p> 
             </div>         
         </div>
     )
